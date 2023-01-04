df3c307851e48edb3a41b0f86c91aa9c9823b332
KathllynLeticia <kathllyn.leticia@gmail.com>
2023-01-03 21:54:10 -0300

    Exercicios Java

---
+++ ExercicosGenJava/src/LacosdeRepeticao/For.java
@@ -0,0 +1,45 @@
+package LacosdeRepeticao;
+
+import java.util.Scanner;
+
+public class For {
+/*Escreva um algoritmo em Java, que leia 10 números inteiros via teclado e mostre 
+ *na tela quantos números são pares e quantos número são ímpares. Veja o exemplo abaixo:
+ * 
+ */
+	
+	// Exercicio 2 usando For
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		
+		int num,somaPar=0,somaImpar=0,x;
+		int idade,genero,categoria,qtdBack=0,qtdMFront=0,qtdHMobile40=0,qtdMFullStack30=0;
+		String op = "S";
+		
+		for (x=1;x<=10;x++)
+		{
+			System.out.println("\nEntre com um número: ");
+			num = leia.nextInt();
+			if (num % 2 == 0)
+			{
+				somaPar++; // somaPar = somaPar + 1
+			}
+			else
+			
+				{
+					somaImpar++;
+				}
+		
+			}
+			System.out.printf("\nTemos: "+somaPar+" números pares...");
+			System.out.printf ("\nTemos: "+somaImpar+" números ímpares...");
+	
+	}			
+			
+}
+
+
+
+	
+
+
