29e5d2a4299089bbdc965181953294cbc04896cb
KathllynLeticia <kathllyn.leticia@gmail.com>
2023-01-09 13:48:33 -0300

    Exercicios Java

---
+++ ExercicosGenJava/src/POOHeranca/TestaGame.java
@@ -0,0 +1,23 @@
+package POOHeranca;
+
+public class TestaGame {
+	
+	public static void main(String[] args) {
+		
+		  
+		Game jessica = new Game ("Jessica!**");
+		jessica.imprimirInfo();
+		Jogo creed = new Jogo ("","Assasins Creed Valhalla", 2020,"RPG Ação","18 anos");
+		creed.visualiza();
+		Console xbox = new Console ("", "Xbox Series S");
+		xbox.visualiza();
+		
+		
+		
+		
+		
+		
+		
+
+}
+}
